45af25e5e040f3160df644075143e199f361c1f3
Anh Khoa <khoanguyen.coder@gmail.com>
2025-01-21 19:28:35 +0700

    up

---
+++ 29/3-imglist.js
@@ -0,0 +1,17 @@
+//img
+(function () {
+  const baseUrl = `../img/29/Mating Dance - Fate/3/`;
+  const imgList = [];
+  for (let i = 1; i <= 38; i++) {
+    imgList.push(`${baseUrl}${i}.jpg`);
+  }
+
+  const imgContainer = document.getElementById("list-img");
+
+  imgList.forEach((imageUrl) => {
+    const img = document.createElement("img");
+    img.src = imageUrl;
+    img.classList.add("img-fluid", "mb-3");
+    imgContainer.appendChild(img);
+  });
+})();
---
+++ 30/2-imglist.js
@@ -0,0 +1,18 @@
+//img
+(function () {
+  const baseUrl = `../img/30/Occhokocchoi Stark-sama to Bouken/2/`;
+  const imgList = [];
+  for (let i = 1; i <= 20; i++) {
+    imgList.push(`${baseUrl}${i}.jpg`);
+  }
+
+
+  const imgContainer = document.getElementById("list-img");
+
+  imgList.forEach((imageUrl) => {
+    const img = document.createElement("img");
+    img.src = imageUrl;
+    img.classList.add("img-fluid", "mb-3");
+    imgContainer.appendChild(img);
+  });
+})();
