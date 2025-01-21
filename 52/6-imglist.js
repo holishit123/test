725612f0a51bac44c9cd376919f92185256453d5
Anh Khoa <khoanguyen.coder@gmail.com>
2025-01-21 22:45:34 +0700

    up

---
+++ 52/6-imglist.js
@@ -0,0 +1,18 @@
+//img
+(function () {
+  const baseUrl = `../img/52/ikuu被催眠洗脑 堕落为母狗性奴的冷艳千金与总裁母亲 - end/6/`;
+  const imgList = [];
+  for (let i = 1; i <= 89; i++) {
+    const formattedNumber = String(i).padStart(3, "0"); // Định dạng số với 3 chữ số, thêm '0' vào trước nếu cần
+    imgList.push(`${baseUrl}06_${formattedNumber}.jpg`);
+  }
+
+  const imgContainer = document.getElementById("list-img");
+
+  imgList.forEach((imageUrl) => {
+    const img = document.createElement("img");
+    img.src = imageUrl;
+    img.classList.add("img-fluid", "mb-3");
+    imgContainer.appendChild(img);
+  });
+})();
