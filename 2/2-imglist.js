fb9597f4e65fc100847d58f98859ac1620310ea8
Anh Khoa <khoanguyen.coder@gmail.com>
2025-01-21 12:13:56 +0700

    2

---
+++ 2/2-imglist.js
@@ -0,0 +1,27 @@
+//img
+const baseUrl = `../img/1/Shiho Nizhizumi - Passion on the Beach/bonus/`;
+const imgList = [
+  `${baseUrl}1.jpg`,
+  `${baseUrl}2.jpg`,
+  `${baseUrl}3.jpg`,
+  `${baseUrl}4.jpg`,
+  `${baseUrl}5.jpg`,
+  `${baseUrl}6.jpg`,
+  `${baseUrl}7.jpg`,
+  `${baseUrl}8.jpg`,
+  `${baseUrl}9.jpg`,
+  `${baseUrl}10.jpg`,
+  `${baseUrl}11.jpg`,
+  `${baseUrl}12.jpg`,
+  `${baseUrl}13.jpg`,
+  `${baseUrl}14.jpg`,
+];
+
+const imgContainer = document.getElementById("list-img");
+
+imgList.forEach((imageUrl) => {
+  const img = document.createElement("img");
+  img.src = imageUrl;
+  img.classList.add("img-fluid", "mb-3");
+  imgContainer.appendChild(img);
+});
---
+++ 2/2.js
@@ -0,0 +1,32 @@
+// theme
+const theme1 = {
+  name: "Shiho Nizhizumi - Passion on the Beach Bonus",
+  theme: `img/1/Shiho Nizhizumi - Passion on the Beach/1/1.jpg`,
+};
+
+const container = document.createElement("div");
+container.classList.add(
+  "col-12",
+  "col-md-6",
+  "col-lg-4",
+  "comic-wrapper",
+  "card"
+);
+const title = document.createElement("h3");
+title.classList.add("card-title", "text-center");
+title.textContent = theme1.name;
+
+const img = document.createElement("img");
+img.classList.add("card-img");
+img.src = theme1.theme;
+img.alt = theme1.name;
+
+container.appendChild(img);
+container.appendChild(title);
+
+document.getElementById("list-comic").appendChild(container);
+
+container.addEventListener("click", function () {
+  console.log("ccc");
+  window.location.href = "1/1.html";
+});
