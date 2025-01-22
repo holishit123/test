116698a647e7971f3292a52ac4ee61750bcdb604
Anh Khoa <khoanguyen.coder@gmail.com>
2025-01-22 21:00:20 +0700

    up

---
+++ 53/3-imglist.js
@@ -0,0 +1,18 @@
+//img
+(function () {
+  const baseUrl = `../img/53/ryota tanaka逆轉/10/`;
+  const imgList = [];
+  for (let i = 1; i <= 100; i++) {
+    const formattedNumber = String(i).padStart(4, "0"); // Định dạng số với 3 chữ số, thêm '0' vào trước nếu cần
+    imgList.push(`${baseUrl}${formattedNumber}.jpg`);
+  }
+
+  const imgContainer = document.getElementById("list-img");
+
+  imgList.forEach((imageUrl) => {
+    const img = document.createElement("img");
+    img.src = imageUrl;
+    img.classList.add("img-fluid", "mb-3");
+    imgContainer.appendChild(img);
+  });
+})();
---
+++ 53/6-imglist.js
@@ -0,0 +1,18 @@
+//img
+(function () {
+  const baseUrl = `../img/53/ryota tanaka逆轉/13/`;
+  const imgList = [];
+  for (let i = 1; i <= 101; i++) {
+    const formattedNumber = String(i).padStart(3, "0"); // Định dạng số với 3 chữ số, thêm '0' vào trước nếu cần
+    imgList.push(`${baseUrl}${formattedNumber}.jpg`);
+  }
+
+  const imgContainer = document.getElementById("list-img");
+
+  imgList.forEach((imageUrl) => {
+    const img = document.createElement("img");
+    img.src = imageUrl;
+    img.classList.add("img-fluid", "mb-3");
+    imgContainer.appendChild(img);
+  });
+})();
