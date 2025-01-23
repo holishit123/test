b454f4d25b31dfaff3fdee0df970139712a709e8
Anh Khoa <khoanguyen.coder@gmail.com>
2025-01-23 20:47:52 +0700

    up

---
+++ 55/5.js
@@ -0,0 +1,33 @@
+// theme
+(function () {
+  const theme1 = {
+    name: "Lust Flight [Awe-some-ryan] - 5",
+    theme: `img/55/Lust Flight [Awe-some-ryan]/5/001.jpg`,
+  };
+
+  const container = document.createElement("div");
+  container.classList.add(
+    "col-12",
+    "col-md-6",
+    "col-lg-4",
+    "comic-wrapper",
+    "card"
+  );
+  const title = document.createElement("h3");
+  title.classList.add("card-title", "text-center");
+  title.textContent = theme1.name;
+
+  const img = document.createElement("img");
+  img.classList.add("card-img");
+  img.src = theme1.theme;
+  img.alt = theme1.name;
+
+  container.appendChild(img);
+  container.appendChild(title);
+
+  document.getElementById("list-comic").appendChild(container);
+
+  container.addEventListener("click", function () {
+    window.location.href = "55/5.html";
+  });
+})();
